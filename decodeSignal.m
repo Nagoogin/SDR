c98163900a4e353e9d29ee84473ee09d7bf2d9ff
Kevin Nguyen <kln4@stmarys-ca.edu>
2015-12-06 23:25:24 -0800

    added decode function for garage door opener

---
+++ decodeSignal.m
@@ -0,0 +1,52 @@
+function [symbols] = decodeSignal(file)
+    data = real(abs(read_complex_binary(file)));
+    chunk = data(1.35e5:3.3e5);
+    N = length(chunk);
+    ndata = NaN(1, N);
+    cutoff = max(chunk)/2;
+    
+    % clean the signal
+    for n = 1:N
+        if chunk(n) > cutoff
+            ndata(n) = 1;
+        else
+            ndata(n) = 0;
+        end
+    end
+    
+    plot(ndata);
+    rise = NaN(1, 66);
+    fall = NaN(1, 66);
+    
+    x = 1;
+    y = 1;
+    for n = 1:N-1
+        if ndata(n) == 0 && ndata(n+1) == 1
+            rise(x) = n;
+            x = x + 1;
+        end
+        if ndata(n) == 1 && ndata(n+1) == 0
+            fall(y) = n;
+            y = y + 1;
+        end
+    end
+    
+    % disp(rise);
+    % disp(length(rise));
+    
+    diff = NaN(1, length(rise));
+    
+    for n = 1:length(rise)
+        diff(n) = fall(n) - rise(n);
+    end
+    
+    symbols = NaN(1, length(diff));
+    
+    for n = 1:length(diff)
+        if diff(n) > 1200
+            symbols(n) = 0;
+        else
+            symbols(n) = 1;
+        end
+    end
+end
